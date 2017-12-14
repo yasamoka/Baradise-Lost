f42851c5a62892d8085e02bfd0f82b8ea6285a24
yasamoka <yasamoka@gmail.com>
2017-12-14 09:42:42 +0200

    experimental support for the plot.ly plotter

--- generate_plot.py
+++ generate_plot.py
@@ -3,14 +3,17 @@ from queue import Queue
 import numpy
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import plotly
 import pickle
 from progressbar import ProgressBar, SimpleProgress, Bar, Timer
 from util import *
+import sys
 
 plot_mode = int(sys.argv[1])
 plot_type = sys.argv[2]
-books_line_numbers_json_filepath = sys.argv[3]
-style_json_filepath = sys.argv[4]
+plotter = sys.argv[3]
+books_line_numbers_json_filepath = sys.argv[4]
+style_json_filepath = sys.argv[5]
 
 if plot_mode == 1:
   num_of_books, book_titles, books_num_of_lines, book_line_numbers_dicts = get_book_line_numbers_mode_1(books_line_numbers_json_filepath)
@@ -54,8 +57,8 @@ else:
 with open(style_json_filepath) as style_json_file:
   style = json.load(style_json_file)
 PLOT_TITLE = style["plot_title"]
-PLOT_XLABEL = style["plot_x_label"]
-PLOT_YLABEL = style["plot_y_label"]
+PLOT_X_LABEL = style["plot_x_label"]
+PLOT_Y_LABEL = style["plot_y_label"]
 PLOT_BAR_GRAPH_HEIGHT = style["plot_bar_graph_height"]
 PLOT_SCATTER_MARKER_STYLE = style["plot_scatter_marker_style"]
 PLOT_SCATTER_MARKER_SIZE = style["plot_scatter_marker_size"]
@@ -147,16 +150,68 @@ if plot_type == "bar":
 
   book_bar_graph_lengths = [0.5] * num_of_books
   progress_bar = ProgressBar(max_value=len(plot_sets), widgets=[SimpleProgress(format='%(value_s)s / %(max_value_s)s'), ' | ', Bar(), Timer()])
-  for i in range(len(plot_sets)):
-    plot_set = plot_sets[i]
-    frequency, values = plot_set
-    values.reverse()
-    color = color_map_hex[frequency]
-    ax.barh(plot_ind, values, PLOT_BAR_GRAPH_HEIGHT, color=color, left=book_bar_graph_lengths)
-    for j in range(num_of_books):
-      book_bar_graph_lengths[j] += values[j]
-    progress_bar.update(i + 1)
-  progress_bar.finish()
+
+  if plotter == "mpl":
+    for i in range(len(plot_sets)):
+      plot_set = plot_sets[i]
+      frequency, values = plot_set
+      values.reverse()
+      color = color_map_hex[frequency]
+      ax.barh(plot_ind, values, PLOT_BAR_GRAPH_HEIGHT, color=color, left=book_bar_graph_lengths)
+      for j in range(num_of_books):
+        book_bar_graph_lengths[j] += values[j]
+      progress_bar.update(i + 1)
+    progress_bar.finish()
+  elif plotter == "py":
+    data = [None] * (len(plot_sets) + num_of_legend_entries)
+    frequency_legend_entry_shown = [False] * num_of_legend_entries
+    for i in range(len(plot_sets)):
+      plot_set = plot_sets[i]
+      frequency, values = plot_set
+      values.reverse()
+      color = 'rgb({})'.format(",".join([str(PLOT_COLOR_MAP[frequency][i]) for i in range(3)]))
+      # aided by https: // stackoverflow.com / questions / 32319619 / disable - hover - information - on - trace - plotly, Shinto Joseph's / MrLeeh's answer
+      showlegend = not frequency_legend_entry_shown[frequency]
+      frequency_legend_entry_shown[frequency] = True
+      bar = plotly.graph_objs.Bar(
+        y=book_titles,
+        #y = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'],
+        x=values,
+        orientation='h',
+        marker=dict(
+          color=color
+        ),
+        showlegend=False,
+        name=labels[frequency],
+        hoverinfo='none'
+      )
+      data[i] = bar
+      progress_bar.update(i + 1)
+    progress_bar.finish()
+    i = 0
+    for frequency in range(num_of_legend_entries - 1, -1, -1):
+      color = 'rgb({})'.format(",".join([str(PLOT_COLOR_MAP[frequency][i]) for i in range(3)]))
+      bar_invisible = plotly.graph_objs.Bar(
+        y=[1],
+        x=[0],
+        orientation='h',
+        marker=dict(color=color),
+        name=labels[frequency],
+        showlegend=True,
+        hoverinfo='none'
+      )
+      data[len(plot_sets) + i] = (bar_invisible)
+      i += 1
+    layout = plotly.graph_objs.Layout(
+      title=PLOT_TITLE,
+      xaxis=dict(title=PLOT_X_LABEL),
+      yaxis=dict(title=PLOT_Y_LABEL),
+      barmode='stack')
+    fig = plotly.graph_objs.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename='test.html')
+    sys.exit(0)
+  else:
+    raise Exception("Invalid plotter ({}).".format(plotter))
 
   coord_formatter = CoordFormatter(book_titles, books_num_of_lines, book_line_numbers_dicts, labels, plot_mode)
   plt.yticks(numpy.arange(num_of_books), book_titles_reversed)
@@ -203,8 +258,8 @@ else:
 ax.format_coord = coord_formatter.format_coord
 
 plt.title(PLOT_TITLE)
-ax.set_ylabel(PLOT_YLABEL)
-ax.set_xlabel(PLOT_XLABEL)
+ax.set_ylabel(PLOT_Y_LABEL)
+ax.set_xlabel(PLOT_X_LABEL)
 frequency_color_patches = [mpatches.Patch(color=color_map_hex[i], label=labels[i]) for i in range(num_of_legend_entries)]
 plt.legend(title=PLOT_LEGEND_TITLE, handles=frequency_color_patches, loc=PLOT_LEGEND_POSITION)
 
